3b6bddba8f16dcdb15a1774a40b6caf72a564d01
jackroo-eng <63950589+jackroo-eng@users.noreply.github.com>
2020-04-19 15:04:28 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,58 @@
+import time
+print("\n"+"-"*45)
+time.sleep(0.5)
+print("\n")
+time.sleep(0.5)
+print("                  Welcome ")
+time.sleep(0.5)
+print("            Ready to calculate?")
+time.sleep(0.5)
+print("                 Jackroo-Eng")
+time.sleep(1)
+print("\n"+"-"*45)
+time.sleep(0.5)
+print("         You can use(+),(-),(*),(/)")
+time.sleep(1)
+print("\n"+"="*45)
+time.sleep(0.5)
+
+frst = float(input("Enter the first number :"))
+print("\n")
+time.sleep(0.5)
+scnd = float(input("Enter the second number :"))
+print("\n")
+time.sleep(0.5)
+
+print("        What operation you want to do?")
+time.sleep(0.5)
+print("                 Addition(+)")
+time.sleep(0.5)
+print("                Subtraction(-)")
+time.sleep(0.5)
+print("               Multiplication(*)")
+time.sleep(0.5)
+print("                  Division(/)")
+time.sleep(0.5)
+
+sign = str(input("Enter your operation!" "\n")) 
+time.sleep(0.5)
+
+print("                Your result is")
+time.sleep(0.5)
+
+if sign=="+" :
+  print ("             ", frst + scnd )
+elif sign=="-" : 
+  print ("             ", frst - scnd )
+elif sign=="*" :
+  print ("             ", frst * scnd )
+elif sign=="/" :
+  print ("             ", frst / scnd )
+else :
+  print("                   Error :(")
+time.sleep(1)
+print("             Thank you for using!")
+time.sleep(0.5)
+print("                       ❤")
+
+  
